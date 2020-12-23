f199285c590ada561e59197e73fcb027890afc38
Jeremy Lancaster <jeremy@consultjl.com>
2020-12-23 12:01:12 -0500

    Updating documentation

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.consultjl.webcrawler"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.consultjl.webcrawler"},{"l":"com.consultjl.webcrawler.saveResult"}]
